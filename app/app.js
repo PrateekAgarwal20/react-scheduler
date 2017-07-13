182c2fc802dcc55465f3ad3cc0d6a2fcddf17940
Prateek-Agarwal-20 <prateeka@sas.upenn.edu>
2017-07-13 10:55:44 -0700

    more set up

--- app/app.js
+++ app/app.js
@@ -1,7 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
 import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers/index.js';
+const store = createStore(reducer);
 
-ReactDOM.render(<App/>,
-   document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
---
+++ app/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// dont forget to import reducers here
+
+// import * as types from '../actions/types';
+
+export default combineReducers({
+    // reducers from other files go here
+});
